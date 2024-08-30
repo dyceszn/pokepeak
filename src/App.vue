34cae98861d3472d90eb067497b55958ee4f6c6d
Duruoha Ikenna <duruohaikenna@gmail.com>
2024-08-30 17:13:53 +0100

    Establish low-fidelity UI framework with basic styling

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,48 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Carousel from "./components/Carousel.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+  <div class="flex w-[100vw] h-full">
+    <main class="w-[55%] h-full"></main>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+    <aside class="flex items-center justify-center w-[45%] h-full p-12">
+      <!-- Rounded Container -->
+
+      <div
+        class="flex flex-col w-full h-full p-16 bg-[var(--primary)] rounded-3xl"
+      >
+        <h1 class="self-start ml-0 justify-self-start">PokéPeak</h1>
+        <div class="flex flex-col items-center w-full h-full justify-evenly">
+          <div class="flex justify-between w-[65%] h-16 text-lg">
+            <button class="w-2/5 border border-black rounded-full">Peak</button>
+            <button class="w-2/5 rounded-full">Log</button>
+          </div>
+          <form
+            action="#"
+            class="flex flex-col self-center justify-between w-[65%] h-[50%]"
+          >
+            <p class="">Discover pokemon in your area.</p>
+            <div class="flex flex-col w-full h-[65%] justify-between">
+              <label for="name">Name</label>
+              <input type="text" id="name" />
+              <label for="level">Level</label>
+              <input type="number" id="level" />
+              <label for="location">Location</label>
+              <input type="text" id="location" />
+              <label for="nick-name">Nickname</label>
+              <input type="text" id="nick-name" />
+            </div>
+            <button
+              type="submit"
+              class="self-end w-16 h-16 border border-black rounded-full"
+            >
+              GO
+            </button>
+          </form>
+          <Carousel />
+        </div>
+      </div>
+    </aside>
+  </div>
+</template>
---
+++ src/components/Carousel.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul
+    class="border-y border-black w-[75%] h-12 flex items-center gap-10 overflow-hidden"
+  >
+    <li>Pikachu</li>
+    <li>Charmander</li>
+    <li>Bulbasaur</li>
+    <li>Squirtle</li>
+    <li>Rattatta</li>
+    <li>JigglyPuff</li>
+    <li>Eevee</li>
+    <li>Snorlax</li>
+    <li>Magikarp</li>
+    <li>Dragonite</li>
+  </ul>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Carousel",
+};
+</script>
